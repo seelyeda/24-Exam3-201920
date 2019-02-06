a091f7ed3cd15c0bf3aa87263502f9b101629a3d
Dave Seelye <seelyeda@rose-hulman.edu>
2019-02-06 15:40:15 -0500

    capstone not exam yet

--- src/problem4.py
+++ src/problem4.py
@@ -5,7 +5,7 @@ Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher,
          Matt Boutell, Amanda Stouder, their colleagues and 
          Dave Seelye.  January 2019.
          
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the   is_prime   function defined below.
+# DONE: 2.  READ the doc-string for the   is_prime   function defined below.
 # It is the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -275,6 +275,21 @@ def problem4(seq_of_seq, n):
     # TODO: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    a = 0
+    for k in range(len(seq_of_seq)):
+        sub = seq_of_seq[k]
+        for j in range(len(sub)):
+            if is_prime(sub[j]) is True:
+                if sub[j] > n:
+                    a = sub[j]
+                    return a
+                else:
+                    a = -1
+            else:
+                a = -1
+    if a == 0:
+        a = -1
+    return a
 
 
 ###############################################################################
